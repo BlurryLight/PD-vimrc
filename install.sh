076d60d1d9d79e43e9045b0ca6c1fbc818877ab4
panda <zhonghaoyu555@gmail.com>
2021-06-21 15:56:07 +0800

    update

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ ln -sf `pwd`/pd.vimrc-base ~/pd.vimrc-base
 ln -sf `pwd`/globalrc ~/.globalrc
 ln -sf `pwd`/.ideavimrc ~/.ideavimrc
 ln -sf `pwd` ~/.vim
+ln -sf `pwd`/tmux.conf ~/.tmux.conf
 # sudo apt install build-essential cmake python3-dev
 # sudo apt install silversearcher-ag
 # sudo apt install ctags gtags

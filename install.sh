b12524b6db8880b4b8a790f6babc392b8c33b4a5
panda <zhonghaoyu555@gmail.com>
2021-06-21 15:18:45 +0800

    update

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash 
 
 cp ~/.vimrc ~/.vimrcbak
-ln -sf `pwd`/pd.vimrc ~/.vimrc
+ln -sf `pwd`/pd.vimrc-youcompleteme ~/.vimrc
+ln -sf `pwd`/pd.vimrc-base ~/pd.vimrc-base
+ln -sf `pwd`/globalrc ~/.globalrc
+ln -sf `pwd`/.ideavimrc ~/.ideavimrc
 ln -sf `pwd` ~/.vim
 # sudo apt install build-essential cmake python3-dev
 # sudo apt install silversearcher-ag

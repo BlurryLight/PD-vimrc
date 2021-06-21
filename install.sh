ff4926315f9978557182cdf73651186ebb5999b4
panda <zhonghaoyu555@gmail.com>
2021-06-21 13:40:10 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash 
-# ubuntu
-ln -sf `pwd`/pd.vimrc ~/.vimrc
-ln -sf `pwd` ~/.vim
-mkdir -p ~/.vim/template
-echo "#!/usr/bin/env python\n#encoding: utf-8\n" > ~/.vim/template/python.tpl
-sudo apt install build-essential cmake python3-dev
-sudo apt install silversearcher-ag
-sudo apt install ctags gtags
+
+cp ~/.vimrc ~/.vimrcbak
+ln -s `pwd`/pd.vimrc ~/.vimrc
+ln -s `pwd` ~/.vim
+# sudo apt install build-essential cmake python3-dev
+# sudo apt install silversearcher-ag
+# sudo apt install ctags gtags

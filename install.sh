ef43e68a38cc53aecd5f08a256b54433217f484f
panda <zhonghaoyu555@gmail.com>
2022-01-11 12:34:14 +0800

    update

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
 #!/bin/bash 
 
-cp ~/.vimrc ~/.vimrcbak
+if [ -e ~/.vimrc ]
+    then cp ~/.vimrc ~/.vimrcpdbak
+ echo "Original .vimrc has been renamed to .vimrcpdbak"
+fi
+
 ln -sf `pwd`/pd.vimrc-youcompleteme ~/.vimrc
 ln -sf `pwd`/pd.vimrc-base ~/pd.vimrc-base
 ln -sf `pwd`/globalrc ~/.globalrc
 ln -sf `pwd`/.ideavimrc ~/.ideavimrc
 ln -sf `pwd` ~/.vim
 ln -sf `pwd`/tmux.conf ~/.tmux.conf
+
+echo "Run vim +PlugInstall"
 # sudo apt install build-essential cmake python3-dev
-# sudo apt install silversearcher-ag
-# sudo apt install ctags gtags
+# sudo apt install silversearcher-ag ripgrep fzf
+# sudo apt install universal-ctags global

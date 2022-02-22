4c6ab10fb58e731dfa0b52e4def2636b671b7401
panda <zhonghaoyu555@gmail.com>
2022-02-22 19:21:20 +0800

    install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,25 @@
-#!/bin/bash 
-
+#!/usr/bin/env bash 
+d=`date +%H_%I_%m-%Y%m%d`
 if [ -e ~/.vimrc ]
-    then cp ~/.vimrc ~/.vimrcpdbak
- echo "Original .vimrc has been renamed to .vimrcpdbak"
+    then cp ~/.vimrc ~/.vimrcpdbak${d}
+ echo "Original .vimrc has been renamed to .vimrcpdbak${d}"
 fi
 
-if [ -e ~/.ctags.d ]
-    then cp ~/.ctags.d. ~/.ctagsbak
-    echo "Original .ctags conf has been renamed to ~/.ctagsbak"
+if [ -d ~/.ctags.d ]
+    then cp -R ~/.ctags.d/ ~/.ctags.d.bak${d}
+    echo "Original .ctags conf has been renamed to ~/.ctags.d.bak${d}"
 else
     mkdir -p ~/.ctags.d
 fi
 
-ln -sf `pwd`/global.ctags ~/.ctags.d/global.ctags
+ln -sfn `pwd`/global.ctags ~/.ctags.d/global.ctags
 
-ln -sf `pwd`/pd.vimrc-youcompleteme ~/.vimrc
-ln -sf `pwd`/pd.vimrc-base ~/pd.vimrc-base
-ln -sf `pwd`/globalrc ~/.globalrc
-ln -sf `pwd`/.ideavimrc ~/.ideavimrc
-ln -sf `pwd` ~/.vim
-ln -sf `pwd`/tmux.conf ~/.tmux.conf
+ln -sfn `pwd`/pd.vimrc-youcompleteme ~/.vimrc
+ln -sfn `pwd`/pd.vimrc-base ~/pd.vimrc-base
+ln -sfn `pwd`/globalrc ~/.globalrc
+ln -sfn `pwd`/.ideavimrc ~/.ideavimrc
+ln -sfn `pwd` ~/.vim
+ln -sfn `pwd`/tmux.conf ~/.tmux.conf
 
 echo "Run vim +PlugInstall"
 # sudo apt install build-essential cmake python3-dev
